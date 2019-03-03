f119d3e84affe65f8f5a48258b18e9a4c31f969b
Travis Gayle <tgayle@users.noreply.github.com>
2019-03-02 19:05:02 -0500

    Abstract TelloDrone into a base Drone class for better testing/documentation (#10)
    * Abstract TelloDrone into a base Drone class for better testing.
    
    Created main.py for one main place for testing/running
    
    * Add documentation to base Drone class and fix incorrect type annotation.

--- tellodroneapi/Drone.py
+++ tellodroneapi/Drone.py
@@ -1,4 +1,7 @@
 class Drone:
+    """
+    A base class representing a drone. Implementations of Drones should be subclasses of this class.
+    """
     async def connect(self) -> bool:
         """
         Attempts to connect to a drone device.
@@ -6,7 +9,7 @@ class Drone:
         """
         raise RuntimeError("Connecting has not been implemented by this drone.")
 
-    async def send_command(self, message: str) -> None:
+    async def send_command(self, message: bytes) -> None:
         """
         Sends a message to a drone device.
         :param message: str The message or command to send to the drone.
